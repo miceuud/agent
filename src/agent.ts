a1b34cf12754008e9aba28d915cb69192dfd1c99
Mike Udoh <miceuud@gmail.com>
2022-10-21 12:53:21 +0100

    Removed Swap Event

--- src/agent.ts
+++ src/agent.ts
@@ -11,14 +11,13 @@ import {
 //  The events can be replaced
 export const ERC20_TRANSFER_EVENT =
   "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const ERC_SWAP_EVENT =
-  "event Swap( address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to)";
 export const ERC_APPROVAL_EVENT =
   "event Approval(address indexed _owner, address indexed _spender, uint256 _value)";
 export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 export const DAI_ERC = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 
 export const WETH_ERC = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+export const GRT_ERC = "0xc944E90C64B2c07662A292be6244BDf05Cda44a7";
 
 let findingsCount = 0;
 
@@ -31,7 +30,7 @@ const handleTransaction: HandleTransaction = async (
   if (findingsCount >= 5) return findings;
 
   // filter the transaction
-  const ercTransferEvents = txEvent.filterLog(ERC20_TRANSFER_EVENT, WETH_ERC);
+  const ercTransferEvents = txEvent.filterLog(ERC20_TRANSFER_EVENT, GRT_ERC);
 
   ercTransferEvents.forEach((transferEvent) => {
     // extract transfer event arguments
